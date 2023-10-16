19a3a1c0be14b9a604b58f42f53657faade00a91
guxiaoai <2372293979@qq.com>
2023-10-16 14:17:53 +0800

    添加项目文件。

---
+++ struct/struct.cpp
@@ -0,0 +1,27 @@
+﻿// struct.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+struct inflatable
+{
+    char name[20];
+    float volume;
+    double price;
+}bouquet,choice;
+int main()
+{
+    bouquet = {
+        "sunflowers",
+        0.20,
+        12.49
+    };
+    //inflatable choice;
+    cout << "bouquet:" << bouquet.name << "for $";
+    cout << bouquet.price <<"\n";
+    choice = bouquet;
+    cout << "choice:" << choice.name << "for $";
+    cout << choice.price << endl;
+    std::cout << "Hello World!\n";
+}
+
---
+++ 单链表/单链表.cpp
@@ -0,0 +1,52 @@
+﻿// 单链表.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+typedef struct LNode
+{
+    int date;
+    LNode* Next;
+}LNode, * LinkList;
+bool InitList(LinkList &L) {
+    L = (LinkList)malloc(sizeof(LNode));
+    if (L == NULL) {
+        return false;  //内存不足的情况
+    }
+    L->Next = NULL;
+    return true;
+}
+LinkList InsertNode(LinkList &L) {
+    int j = 9999;
+    std::cin >> j;
+    while (j != 9999) {
+        LNode* s = (LNode*)malloc(sizeof(LNode));
+        s->date = j;
+        s->Next = L->Next;
+        L->Next = s;
+        std::cin >> j;
+    }
+    return L;
+}
+int PrintLinkList(LinkList L) {
+    LNode* x = (LNode*)malloc(sizeof(LNode));
+    if(L->Next != NULL)
+    {
+        x = L->Next;
+        std::cout << x->date << std::endl;
+        PrintLinkList(x);
+    }
+    else
+    {
+        return 9999;
+    }
+}
+int main()
+{
+    LinkList L;
+    InitList(L);
+    std::cout << "请输入正整数后确认\n";
+    InsertNode(L);
+    PrintLinkList(L);
+    std::cout << "Hello World!\n";
+}
