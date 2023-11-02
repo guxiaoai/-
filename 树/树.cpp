ef66c60e7212c8625f8b9faf905edced1e730fe2
guxiaoai <2372293979@qq.com>
2023-11-02 12:06:13 +0800

    树

---
+++ 树/树.cpp
@@ -0,0 +1,20 @@
+﻿#include<stddef.h>
+#include <iostream>
+using namespace std;
+#define MaxSize 100;
+struct TreeNode {
+	int value;
+	bool isEmpty;
+};
+typedef struct _BitNode {
+	int data;
+	struct _BitNode* lChild, * rChild;
+}*BitTree, BitNode;
+void InsertNewNode(BitTree T,int x){
+	BitNode* p = (BitNode*)malloc(sizeof(BitNode));
+}
+
+void main(){
+	BitTree Root = NULL;
+	
+}
